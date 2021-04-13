3c766c448041057f619177314a5bb832e63c6e25
Alexander Fok <foktmalexander@gmail.com>
2021-04-12 19:05:09 -0700

    Add reference to mapSize

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,6 +1,8 @@
+import mapSize from './vars/mapSize';
+
 const Map = () => {
-  let initMap = new Array(45).fill(new Array(64).fill(0));
-  initMap[22] = new Array(64).fill(1);
+  let initMap = new Array(mapSize.y).fill(new Array(mapSize.x).fill(0));
+  initMap[22] = new Array(mapSize.x).fill(1);
   return initMap;
 }
 
