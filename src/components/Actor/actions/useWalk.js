eeb84542091be615984b5f55c93084f3dde15fe8
Alexander Fok <foktmalexander@gmail.com>
2021-04-12 20:56:24 -0700

    Add character size

--- src/components/Actor/actions/useWalk.js
+++ src/components/Actor/actions/useWalk.js
@@ -4,7 +4,7 @@ import movements, {stepSize} from '../vars/movements';
 import mapSize from '../../Map/vars/mapSize';
 
 // Get numeric express for character's step and dir
-const useWalk = (initPos={ x:0, y:0 }, map) => {
+const useWalk = (initPos={ x:0, y:0 }, map, charSize) => {
   const [position, setPosition] = useState(initPos);
   const [step, setStep] = useState(0);
   const [dir, setDir] = useState(0);
@@ -15,7 +15,11 @@ const useWalk = (initPos={ x:0, y:0 }, map) => {
         x: prev.x + movements[dir].x,
         y: prev.y + movements[dir].y
       }
-      const isBoundary = next.x < 0 || next.x > mapSize.x - 1 || next.y < 0 || next.y > mapSize.y - 1;
+      const offset = {
+        x: mapSize.x - 1 - charSize.width,
+        y: mapSize.y - 1 - charSize.height
+      }
+      const isBoundary = next.x < 0 || next.x > offset.x || next.y < 0 || next.y > offset.y;
       if (isBoundary) return prev;
       
       const isWall = map[next.y][next.x] === 1;
@@ -24,7 +28,6 @@ const useWalk = (initPos={ x:0, y:0 }, map) => {
   }
 
   const walk = (dir) => {
-    if(!directions.hasOwnProperty(dir)) return;
     setStep((prev) => prev < 2 ? prev + 1 : 0);
     setDir((prev) => {
       if (prev === directions[dir]) move(dir);
@@ -36,6 +39,10 @@ const useWalk = (initPos={ x:0, y:0 }, map) => {
     position: {
       x: position.x * stepSize,
       y: position.y * stepSize,
+    },
+    size: {
+      width: charSize.width * stepSize,
+      height: charSize.height * stepSize
     },
     step,
     dir,
--- src/components/Actor/index.js
+++ src/components/Actor/index.js
@@ -1,9 +1,14 @@
 import Sprite from '../Sprite';
 import { DOWN } from './vars/directions';
 
-const Actor = ({image, position={x: 0, y: 0}, step = 0, dir=DOWN}) => {
-  const width = 32;
-  const height = 32;
+const Actor = ({
+  image,
+  size={width: 32, height: 32},
+  position={x: 0, y: 0},
+  step = 0,
+  dir=DOWN
+}) => {
+  const {width, height} = size;
   return (
     <Sprite
       image = {image}
